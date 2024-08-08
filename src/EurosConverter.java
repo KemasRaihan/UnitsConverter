c85ffc6b589e424be52631217b3d8c0b8e167d39
KemasRaihan <b6024493@my.shu.ac.uk>
2024-08-08 12:50:03 +0100

    Git init

---
+++ src/EurosConverter.java
@@ -0,0 +1,20 @@
+public class EurosConverter implements Converter{
+    public EurosConverter(){
+    }
+
+    @Override
+    public double convertToDollars(double amount){
+        return amount * 1.09;
+    }
+
+    @Override
+    public double convertToSterling(double amount){
+        return amount * 0.86;
+    }
+
+    @Override
+    public double convertToEuros(double amount){
+        return amount;
+    }
+
+}
